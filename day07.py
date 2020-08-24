49d84cc910b520152e44d72fef49a1ecd4fd649a
Amber-otto <RA_master@163.com>
2020-08-24 20:41:06 +0800

    2020/08/24

---
+++ day07.py
@@ -0,0 +1,20 @@
+# l = [ i**2 for i in range(30+1) if i % 3 is 0]
+# print(l)
+
+# names = [['Tom', 'Billy', 'Jefferson', 'Andrew', 'Wesley', 'Steven', 'Joe'],
+#          ['Alice', 'Jill', 'Ana', 'Wendy', 'Jennifer', 'Sherry', 'Eva']]
+#
+# name1 = []
+# for i in names:
+#     for x in i:
+#         num = 0
+#         for j in x:
+#             if j == "e":
+#                 num += 1
+#                 if num == 2:
+#                     name1.append(x)
+# print(name1)
+#
+# name1 = [ x for i in names for x in i if x.count("e") >= 2 ]
+# print(name1)
+
